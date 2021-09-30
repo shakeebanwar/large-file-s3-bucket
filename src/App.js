c8311348a5b15517b76372345b24aaf462ce30eb
muhammadharis1 <mh7365496@gmail.com>
2021-09-30 16:48:55 +0500

    done

--- src/App.js
+++ src/App.js
@@ -75,6 +75,79 @@ export default App;
 
 
 
+// import { Upload } from "@aws-sdk/lib-storage"
+// import {S3Client,S3, Progress,AbortMultipartUploadCommand } from "@aws-sdk/client-s3"
+// import React, { useState } from 'react'
+
+
+// function App() {
+
+//   const [progress , setProgress] = useState(0);
+
+//   const  upload = (file) => {
+//     console.log("start uploading...")
+//     var file = file.target.files[0]
+//     const target = {Bucket:"shakeeb-crm",Key:`video/${file.name}`,Body:file}
+//     const creds = {accessKeyId: 'AKIA57IYNILPJ45CH7JK',secretAccessKey: '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'}
+//     try{
+//       const parallelUploads3 =new Upload({
+
+//         client: new S3({region:"us-east-2",credentials:creds}) || new S3Client({region:"us-east-2",credentials:creds}),
+//         // client: new S3Client({region:"us-east-2",credentials:creds}),
+//         // queueSize:1,
+//         // partSize:1024 * 1024 * 5,
+//         leavePartsOnError:false,
+//         params:target,
+        
+//       })
+
+//       parallelUploads3.on("httpUploadProgress",(progress)=>{
+//         console.log("progress",progress)
+//         setProgress(Math.round((progress.loaded / progress.total) * 100))
+//       });
+
+//   parallelUploads3.done()
+
+
+//     }
+//     catch(e){
+//       console.log("exception ",e)
+
+//     }
+
+//   }
+
+//   return (
+//     <div>
+
+//       <div>Native SDK File Upload Progress is {progress}%</div>
+
+//       <input type="file" onChange={upload}/>  
+
+
+//     </div>
+
+//   )
+
+
+
+// }
+
+// export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
